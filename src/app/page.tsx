06dc4f8bd6a8908e0b707cfd7b7581e9a92c76af
Ehiane <e.oigiagbe@wsu.edu>
2024-05-08 07:31:31 -0700

    Added 2 new components, the navbar and search box

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
+import Navbar from "@/components/Navbar";
 import Image from "next/image";
 
 export default function Home() {
-  return <div>Hello</div> 
+  return <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
+    <Navbar/>
+    </div> 
 }
